feb557f14820a26b2deae794db0c196643f75466
Yenebeb Daniel Fikre <r0901225@student.thomasmore.be>
2022-10-25 10:44:04 +0200

    Add project files.

---
+++ EGakko_DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EGakko_Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGakko_DAL.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<CustomUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
